0be90399acb4de582b05fe7d8d804ae2fd3c294f
adamMcneil <adamwmcneil@gmail.com>
2021-12-07 22:31:01 -0600

    it nearly works just working with copy

---
+++ Cell.py
@@ -0,0 +1,56 @@
+import pygame
+import random
+
+
+class Cell:
+
+    def __init__(self, location, size, alive=False):
+        self.alive_color = (0, 0, 0)
+        self.dead_color = (255, 255, 255)
+        self.alive = alive
+        self.location = location
+        self.size = size
+        self.index = (location[0] // size, location[1] // size)
+        self.shape = pygame.Rect(location[0], location[1], size, size)
+
+    def print_cell(self, screen):
+        if self.alive:
+            # self.set_random_color()
+            # self.set_random_color()
+            pygame.draw.rect(screen, self.alive_color, self.shape, 0, 0)
+        else:
+            pygame.draw.rect(screen, self.dead_color, self.shape, 0, 0)
+
+    def set_random_color(self):
+        top = 155
+        bottom = 100
+        self.alive_color = (random.randint(bottom, top), random.randint(bottom, top), random.randint(bottom, top))
+
+    def check(self, cells):
+        num = self.get_num_of_alive_neighbors(cells)
+        if num == 3:
+            self.alive = True
+            return
+        if num == 2:
+            return
+        self.alive = False
+
+
+    def get_num_of_alive_neighbors(self, cells):
+        check_nums = [[-1, -1],
+                      [-1, 0],
+                      [-1, 1],
+                      [0, -1],
+                      [0, 1],
+                      [1, -1],
+                      [1, 0],
+                      [1, 1]
+                      ]
+        count = 0
+        for pair in check_nums:
+            x = self.index[0] + pair[0]
+            y = self.index[1] + pair[1]
+            if (0 <= (self.index[0] + pair[0]) < len(cells[0])-1) and (0 <= (self.index[1] + pair[1]) < len(cells)-1):
+                if cells[self.index[0] + pair[0]][self.index[1] + pair[1]].alive:
+                    count += 1
+        return count
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import Cell
+import time
+import random
 
 pygame.init()
 
@@ -8,13 +11,52 @@ white = (255, 255, 255)
 
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 size = 10
+cells = [[]]
+for x in range(0, screenWidth-1, size):
+    row = []
+    cells.append(row)
+    for y in range(0, screenHeight-1, size):
+        cell = Cell.Cell([x, y], size)
+        # rand_num = random.randint(0,1)
+        # if rand_num == 1:
+        #     cell.alive = True
+        # else:
+        #     cell.alive = False
+        cells[x//size].append(cell)
+cells[50][50].alive = True
+cells[51][51].alive = True
+cells[51][52].alive = True
+cells[50][52].alive = True
+cells[49][52].alive = True
+
+# print(cells)
+def copy_cells(cells):
+    copy = [[]]
+    for rows in cells:
+        row = []
+        copy.append(row)
+        for cell in rows:
+            copy_cell = Cell.Cell(cell.location, cell.size, cell.alive)
+            row.append(copy_cell)
+    return copy
 
 
 running = True
 while running:
     events = pygame.event.get()
-    screen.fill(white)
+    # screen.fill(white)
     for event in events:
         if event.type == pygame.QUIT:
             running = False
+    copy = copy_cells(cells)
+    # print(type(copy))
+    # print(type(copy[0]))
+    # print(type(copy[0][0]))
+    for row in cells:
+        for cell in row:
+            cell.print_cell(screen)
+            cell.check(copy)
+            # print(cell.alive)
+
     pygame.display.update()
+    time.sleep(0)
